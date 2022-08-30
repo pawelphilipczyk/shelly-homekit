5ee903ce81cad2675d9fc5c6168f4cf6ec2987ca
Paweł Philipczyk <pawel.philipczyk@me.com>
2022-08-30 23:54:30 +0200

    restore app functionality in vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,109 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useCallback, useEffect, useState } from "react";
+import type { ShellyDevice } from "./types";
+import { fetchWithTimeout } from "./utils/fetchWithTimeout";
+import "./App.css";
+
+const getIpsRange = () => {
+  const ips: string[] = [];
+  for (let i = 0; i <= 255; i++) ips.push(`192.168.88.${i}`);
+  return ips;
+};
+
+const isShellyDevice = (device: ShellyDevice) => Boolean(device.device_id);
+
+const getStoredDevices = () =>
+  JSON.parse(localStorage.getItem("shelly-devices") || "[]");
+
+const setStoredDevices = (devices: ShellyDevice[]) =>
+  localStorage.setItem("shelly-devices", JSON.stringify(devices));
+
+const fetchShelly = (ip: string): Promise<ShellyDevice> => {
+  const url = `http://${ip}/rpc/Shelly.GetInfo`;
+
+  return fetchWithTimeout(url)
+    .then((response) => response.json())
+    .catch((error) => new Error(error));
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [devices, setDevices] = useState<ShellyDevice[]>([]);
+  const [isPending, setPending] = useState(false);
+
+  const scanDevices = useCallback(() => {
+    setPending(true);
+    const ips = getIpsRange();
+
+    const fetchIpBatch = () => {
+      const ipsBatch = ips.splice(0, 10);
+      if (ipsBatch.length > 0) {
+        Promise.all(ipsBatch.map(fetchShelly))
+          .then((responses) => responses.filter(isShellyDevice))
+          .then((devices) =>
+            setDevices((prevState) => [...prevState, ...devices])
+          )
+          .finally(fetchIpBatch);
+      } else {
+        setPending(false);
+        setDevices((devices) => {
+          setStoredDevices(devices);
+          return devices;
+        });
+      }
+    };
+    fetchIpBatch();
+  }, []);
+
+  const refreshDevices = () => {
+    localStorage.removeItem("shelly-devices");
+    setDevices([]);
+    scanDevices();
+  };
+
+  useEffect(() => {
+    const storedDevices = getStoredDevices();
+    if (storedDevices.length) setDevices(storedDevices);
+    else scanDevices();
+  }, [scanDevices]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.4",
+        padding: "2em",
+      }}
+    >
+      <header>
+        <h1>Shelly Homekit Dashboard</h1>
+      </header>
+      <section>
+        {isPending && <p>Scanning local network...</p>}
+        <button disabled={isPending} onClick={refreshDevices}>Refresh</button>
+      </section>
+      <h2>Found {devices.length} devices</h2>
+      <section>
+        {devices.map((device) => (
+          <form key={device.name}>
+            <fieldset>
+              <h3>{device.name}</h3>
+              <dl>
+                <dt>Host</dt>
+                <dd>
+                  <a
+                    href={`http://${device.host}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {device.host}
+                  </a>
+                </dd>
+              </dl>
+            </fieldset>
+          </form>
+        ))}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
